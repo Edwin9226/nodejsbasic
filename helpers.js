90f9148d1420b32618fadac3c9460c7a28149a4e
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-07-14 00:56:01 -0500

    exports arrow fuction

---
+++ helpers.js
@@ -0,0 +1,13 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// arrow fuction
+exports.sum = (a, b) => a + b;
+
+
+// module.exports = {
+//     sum
+// };
+
+// console.log("PROCESS:", process)
