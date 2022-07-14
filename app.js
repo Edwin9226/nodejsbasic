c66c9d57384add6688c432f8a08c496eef4fb990
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-07-14 02:14:44 -0500

    callback and Asincrnomous

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const { sum } = require('./helpers');
+// const { sum } = require('./helpers');
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hey whas up")
-});
+// app.get('/', (req, res) => {
+//     res.send("hey whas up")
+// });
 
-app.listen(3000);
-// var let const
-const total = sum(10, 200);
-console.log("Total:", total);
+// app.listen(3000);
+// // var let const
+// const total = sum(10, 200);
+// console.log("Total:", total);
+
+const fs = require('fs');
+const fileName = "target.txt"
+
+// fs.watch(fileName, () => console.log("File changed!"));
+
+fs.readFile(fileName, (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(data.toString())
+})
+
+console.log("Node js Async programing.")
